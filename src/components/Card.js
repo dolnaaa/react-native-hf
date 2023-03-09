2ddaffc4c645721a3ae3cbdfcd14ab957d12739e
dolnaaa <matedolnik@gmail.com>
2023-03-09 15:33:51 +0100

    feat: Card component made

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,63 @@
-import { Text, View } from 'react-native'
-import React, { Component } from 'react'
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import React, { Component } from "react";
+import { Ionicons } from "@expo/vector-icons";
 
 export class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
-      <View>
-        <Text>Card</Text>
+      <View style={styles.mainContainer}>
+        <Text style={styles.title}>
+          {this.props.name ? this.props.name : "err"}
+        </Text>
+        <Text style={styles.shortDescription}>
+          {this.props.shortDescription ? this.props.shortDescription : "err"}
+        </Text>
+        <View style={styles.bottomContainer}>
+          <TouchableOpacity onPress={() => console.log("action")}>
+            <Ionicons name="pencil" size={24} color="black" />
+          </TouchableOpacity>
+          <Text style={styles.count}>
+            {this.props.count ? this.props.count : "err"}
+          </Text>
+          <TouchableOpacity onPress={() => console.log("action")}>
+            <Ionicons name="trash" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
       </View>
-    )
+    );
   }
 }
 
-export default Card
+const styles = StyleSheet.create({
+  mainContainer: {
+    marginBottom: 20,
+    padding: 10,
+    paddingHorizontal: 20,
+    flexDirection: "column",
+    backgroundColor: "#ddd",
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  shortDescription: {
+    fontSize: 14,
+    marginVertical: 8,
+  },
+  count: {
+    fontSize: 14,
+  },
+  bottomContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+});
+
+export default Card;

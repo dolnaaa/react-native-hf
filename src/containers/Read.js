b64dc5b3edb3b07045915bf4260dd7191f01d8c6
dolnaaa <matedolnik@gmail.com>
2023-03-09 15:34:15 +0100

    feat: Read now renders Cards from dummydata

--- src/containers/Read.js
+++ src/containers/Read.js
@@ -1,11 +1,29 @@
-import { Text, View, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import React, { Component } from "react";
+import Card from "../components/Card";
+
+const dummyData = [
+  { id: 1, name: "A item", shortDescription: "This is the item A", count: 3 },
+  { id: 2, name: "B item", shortDescription: "This is the item B", count: 2 },
+  { id: 3, name: "C item", shortDescription: "This is the item C", count: 8 },
+  { id: 4, name: "D item", shortDescription: "This is the item D", count: 6 },
+];
 
 export class Read extends Component {
   render() {
     return (
       <View style={styles.mainContainer}>
-        <Text>Read</Text>
+        <ScrollView style={styles.mainScrollContainer}>
+          {dummyData?.map((item) => (
+            <Card
+              key={item?.id}
+              id={item?.id}
+              name={item?.name}
+              shortDescription={item?.shortDescription}
+              count={item?.count}
+            />
+          ))}
+        </ScrollView>
       </View>
     );
   }
@@ -14,6 +32,10 @@ export class Read extends Component {
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
+  },
+  mainScrollContainer: {
+    flex: 1,
+    padding: 10,
   },
 });
 
